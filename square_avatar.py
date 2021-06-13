d4789f50de5c44490d384e4ac3efbb99f6dea706
Alexandre Vieira <59124659+JGSVb@users.noreply.github.com>
2021-06-14 00:42:21 +0100

    Add files via upload

--- square_avatar.py
+++ square_avatar.py
@@ -19,7 +19,6 @@ class CONFIG:
     output_folder = "output"
 
 
-
 def mask_image(mask_image : Image.Image, image : Image.Image):
 
     mask_image = mask_image.convert("L")
@@ -67,14 +66,27 @@ def image_chooser_dialog(title = "Escolher Imagem"):
         return filename
     return None
 
+def cairo_rounded_rectangle(ctx : cairo.Context, x : float, y : float, width : float, height : float, radius : float):
+    # https://www.cairographics.org/samples/rounded_rectangle/
+    # Cairo é complicado então utilizei esse código :thumbsup:
+
+    radius = radius
+    degrees = pi / 180
+
+    x -= width / 2
+    y -= height / 2
+
+    ctx.new_sub_path()
+    ctx.arc(x + width - radius, y + radius, radius, -90 * degrees, 0 * degrees)
+    ctx.arc(x + width - radius, y + height - radius, radius, 0 * degrees, 90 * degrees)
+    ctx.arc(x + radius, y + height - radius, radius, 90 * degrees, 180 * degrees)
+    ctx.arc(x + radius, y + radius, radius, 180 * degrees, 270 * degrees)
+    ctx.close_path()
 
 
 class AvatarEditor(Gtk.DrawingArea):
     def __init__(self, *args, **kwargs):
         Gtk.DrawingArea.__init__(self, can_focus = True)
-
-        self.__avatar_image = None
-        self.__image_surface = None
 
         self.add_events(
             Gdk.EventMask.BUTTON_PRESS_MASK |
@@ -84,9 +96,16 @@ class AvatarEditor(Gtk.DrawingArea):
             Gdk.EventMask.KEY_PRESS_MASK
         )
 
+        self.__avatar_image = None
+        self.__image_surface = None
+
         self.__button_pressed = -1
         self.__button_value = -1
         self.__mouse_x = self.__mouse_y = -1
+
+        self.__circle_radius_increment = 5
+        self.__border_radius = 10
+        self.__image_zoom = 0.10
 
         self.connect("button-press-event", self.button_event)
         self.connect("button-release-event", self.button_event)
@@ -94,6 +113,53 @@ class AvatarEditor(Gtk.DrawingArea):
         self.connect("scroll-event", self.scroll_event)
 
         self.connect("draw", self.draw)
+
+    @GObject.Property(type = int, minimum = 0)
+    def circle_radius_increment(self):
+        return self.__circle_radius_increment
+    @circle_radius_increment.setter
+    def circle_radius_increment(self, val):
+        self.__circle_radius_increment = val
+        self.__circle_radius = 0
+
+    @GObject.Property(type = float, minimum = 0)
+    def image_zoom(self):
+        return self.__image_zoom
+    @image_zoom.setter
+    def image_zoom(self, val):
+        self.__image_zoom = val
+
+    @GObject.Property(type = float, minimum = 0)
+    def border_radius(self):
+        return self.__border_radius
+    @border_radius.setter
+    def border_radius(self, val):
+        self.__border_radius = val
+        self.queue_draw()
+
+    @GObject.Property(type = float)
+    def circle_x(self):
+        return self.__circle_x
+    @GObject.Property(type = float)
+    def circle_y(self):
+        return self.__circle_y
+    @GObject.Property(type = float)
+    def circle_radius(self):
+        return self.__circle_radius
+
+    @GObject.Property(type = float)
+    def image_surface_x(self):
+        return self.__image_surface_x
+    @GObject.Property(type = float)
+    def image_surface_y(self):
+        return self.__image_surface_y
+
+    @GObject.Property(type = float)
+    def image_surface_width(self):
+        return self.__image_surface_width
+    @GObject.Property(type = float)
+    def image_surface_height(self):
+        return self.__image_surface_height
 
     def get_avatar_image(self):
         return self.__avatar_image
@@ -131,7 +197,7 @@ class AvatarEditor(Gtk.DrawingArea):
     def image_surface_scale(self, width : float, height : float) -> (float, float):
 
         scaled = self.__image_pixbuf.copy()
-        scaled = scaled.scale_simple(width, height, GdkPixbuf.InterpType.BILINEAR)
+        scaled = scaled.scale_simple(width, height, GdkPixbuf.InterpType.TILES)
         surface = Gdk.cairo_surface_create_from_pixbuf(scaled, 0, None)
 
         wb, hb = self.__image_surface_width, self.__image_surface_height
@@ -143,24 +209,28 @@ class AvatarEditor(Gtk.DrawingArea):
     def image_surface_proportional_scale(self, fac : float) -> (float, float):
         return self.image_surface_scale(self.__image_surface_width * fac, self.__image_surface_height * fac)
 
+    def center_image_surface(self):
+        scaled = self.__image_pixbuf.copy()
+
+        # Imagem é horizontal
+        if self.__image_width > self.__image_height:
+            width = self.__widget_width
+            height = width * self.__image_height_ratio
+            self.__image_surface_y = self.__widget_height / 2 - height / 2
+
+        # Imagem é vertical ou quadrada
+        else:
+            height = self.__widget_height
+            width = height * self.__image_width_ratio
+            self.__image_surface_x = self.__widget_width / 2 - width / 2
+
+        self.image_surface_scale(width, height)
+
     def do_realize(self):
         Gtk.DrawingArea.do_realize(self)
 
         self.update_widget_dim()
-
-        scaled = self.__image_pixbuf.copy()
-
-        width = self.__widget_width
-        height = width * self.__image_height_ratio
-        self.__image_surface_y = self.__widget_height / 2 - height / 2
-
-        if width > self.__widget_width or height > self.__widget_height:
-            height = self.__widget_height
-            width = height * self.__image_width_ratio
-            self.__image_surface_y = 0
-            self.__image_surface_x = self.__widget_width / 2 - width / 2
-
-        self.image_surface_scale(width, height)
+        self.center_image_surface()
 
         self.__circle_radius = min(self.__widget_width, self.__widget_height) / 2 - 40
         self.__circle_x, self.__circle_y = self.__widget_width / 2, self.__widget_height / 2
@@ -188,25 +258,25 @@ class AvatarEditor(Gtk.DrawingArea):
 
             # Zoom no raio do círulo
             if event.direction == Gdk.ScrollDirection.UP:
-                self.__circle_radius += 3
+                self.__circle_radius += self.__circle_radius_increment
             elif event.direction == Gdk.ScrollDirection.DOWN:
-                self.__circle_radius -= 3
+                self.__circle_radius -= self.__circle_radius_increment
 
         else:
             # Zoom na imagem
             if event.direction == Gdk.ScrollDirection.UP:
 
-                wb, hb = self.image_surface_proportional_scale(1.10)
+                wb, hb = self.image_surface_proportional_scale(1 + self.__image_zoom)
 
-                self.__image_surface_x -= (self.__mouse_x - self.__image_surface_x) * (1.10 - 1)
-                self.__image_surface_y -= (self.__mouse_y - self.__image_surface_y) * (1.10 - 1)
+                self.__image_surface_x -= (self.__mouse_x - self.__image_surface_x) * (self.__image_zoom)
+                self.__image_surface_y -= (self.__mouse_y - self.__image_surface_y) * (self.__image_zoom)
 
             elif event.direction == Gdk.ScrollDirection.DOWN:
 
-                wb, hb = self.image_surface_proportional_scale(0.90)
+                wb, hb = self.image_surface_proportional_scale(1 - self.__image_zoom)
 
-                self.__image_surface_x -= (self.__mouse_x - self.__image_surface_x) * (0.90 - 1)
-                self.__image_surface_y -= (self.__mouse_y - self.__image_surface_y) * (0.90 - 1)
+                self.__image_surface_x -= (self.__mouse_x - self.__image_surface_x) * (1 - self.__image_zoom - 1)
+                self.__image_surface_y -= (self.__mouse_y - self.__image_surface_y) * (1 - self.__image_zoom - 1)
 
         self.queue_draw()
 
@@ -227,20 +297,24 @@ class AvatarEditor(Gtk.DrawingArea):
 
         if self.__image_surface:
 
+            # Fundo
             ctx.set_source_rgb(0.1, 0.1, 0.1)
             ctx.rectangle(0, 0, self.__widget_width, self.__widget_height)
             ctx.fill()
 
+            # Quadrado de escurecimento
             ctx.set_source_rgba(0.20, 0.23, 0.28)
             ctx.rectangle(self.__image_surface_x, self.__image_surface_y, self.__image_surface_width, self.__image_surface_height)
             ctx.fill()
 
+            # Buraco no quadrado
             ctx.set_source_rgb(1, 1, 1)
-            ctx.arc(self.__widget_width / 2, self.__widget_height / 2,
-                    self.__circle_radius,
-                    0, 2 * PI)
+            cairo_rounded_rectangle(ctx, self.__circle_x, self.__circle_y,
+                                    self.__circle_radius, self.__circle_radius + 10,
+                                    self.__border_radius)
             ctx.fill()
 
+            # Imagem
             ctx.set_operator(cairo.OPERATOR_MULTIPLY)
             ctx.set_source_surface(self.__image_surface, self.__image_surface_x, self.__image_surface_y)
             ctx.paint()
